265cbef7c6983db2dad4709068c02e5ca1f58d8e
Beasheng <Adverial.application@gmail.com>
2024-04-30 10:17:17 +0300

    update settings

--- app/src/main/java/com/application/adverial/MainActivity.kt
+++ app/src/main/java/com/application/adverial/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.application.adverial
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.application.adverial.service.SharedPrefManager
 import com.application.adverial.service.Tools
 import com.application.adverial.ui.navigation.Home
 import com.application.adverial.utils.DialogUtils
 import com.application.adverial.utils.NetworkUtils
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         val imgr: InputMethodManager =
             getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         imgr.hideSoftInputFromWindow(this.window.decorView.rootView.windowToken, 0)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Enable cleartext traffic
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .permitNetwork().build()
+            )
+        }
     }
 
     override fun onResume() {
@@ -31,9 +42,9 @@ class MainActivity : AppCompatActivity() {
             window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
         }
 
-        val sharedPrefManager: SharedPrefManager by lazy {
-            SharedPrefManager(this)
-        }
+//        val sharedPrefManager: SharedPrefManager by lazy {
+//            SharedPrefManager(this)
+//        }
 
 
         if (NetworkUtils.isNetworkAvailable(this)) {

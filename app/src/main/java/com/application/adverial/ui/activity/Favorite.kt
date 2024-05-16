d52a70229cf9ddddb181df3910fd40af02b664e6
Beasheng <Adverial.application@gmail.com>
2024-05-16 10:13:23 +0300

    chore: Update language dialog layout and item order

--- app/src/main/java/com/application/adverial/ui/activity/Favorite.kt
+++ app/src/main/java/com/application/adverial/ui/activity/Favorite.kt
@@ -1,27 +1,30 @@
 package com.application.adverial.ui.activity
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.application.adverial.R
 import com.application.adverial.remote.Repository
 import com.application.adverial.remote.model.FavoriteData
 import com.application.adverial.service.Tools
 import com.application.adverial.ui.adapter.FavoriteAdapter
-import kotlinx.android.synthetic.main.activity_favorite.*
+import kotlinx.android.synthetic.main.activity_favorite.favoriteRoot
+import kotlinx.android.synthetic.main.activity_favorite.favorite_recyclerView
 import kotlinx.android.synthetic.main.activity_favorite.lottie6
+import kotlinx.android.synthetic.main.activity_favorite.notification_no2
 
 class Favorite : AppCompatActivity() {
 
     private var itemList= ArrayList<FavoriteData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Favorite", "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
-        Tools().rotateLayout(this,favoriteRoot)
+//        Tools().rotateLayout(this,favoriteRoot)
         Tools().changeViewFromTheme(this,favoriteRoot)
     }
 
@@ -55,7 +58,9 @@ class Favorite : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         pageInit()
+        Log.d("Favorite", "onResume")
         Tools().getLocale(this)
+//        Tools().rotateLayout(this,favoriteRoot)
         val language =  getSharedPreferences("user", 0).getString("languageId", "")
         if (language == "" ||language == "0" || language == "1") window.decorView.layoutDirection= View.LAYOUT_DIRECTION_LTR
         else window.decorView.layoutDirection= View.LAYOUT_DIRECTION_RTL

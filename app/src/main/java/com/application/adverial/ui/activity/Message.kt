825847553aabbafb4241f3a46e999c59911ccb1b
Zeyad Khadeeda <info@lelav.com>
2024-07-13 14:09:39 +0300

    feat: Add MessageActivity to handle messaging functionality

--- app/src/main/java/com/application/adverial/ui/activity/Message.kt
+++ app/src/main/java/com/application/adverial/ui/activity/Message.kt
@@ -14,24 +14,24 @@ class Message : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
-        Tools().rotateLayout(this,imageView44)
-        Tools().changeViewFromTheme(this,messageRoot)
+//        Tools().rotateLayout(this,imageView44)
+//        Tools().changeViewFromTheme(this,messageRoot)
 
-        pageInit()
+//        pageInit()
     }
 
-    private fun pageInit(){
-        message_recyclerView.layoutManager= LinearLayoutManager(this)
-        message_recyclerView.adapter= MessageAdapter()
-    }
-
-    fun back(view: View){ finish() }
-
-    override fun onResume() {
-        super.onResume()
-        Tools().getLocale(this)
-        val language =  getSharedPreferences("user", 0).getString("languageId", "")
-        if (language == "" ||language == "0" || language == "1") window.decorView.layoutDirection= View.LAYOUT_DIRECTION_LTR
-        else window.decorView.layoutDirection= View.LAYOUT_DIRECTION_RTL
-    }
+//    private fun pageInit(){
+//        message_recyclerView.layoutManager= LinearLayoutManager(this)
+//        message_recyclerView.adapter= MessageAdapter()
+//    }
+//
+//    fun back(view: View){ finish() }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        Tools().getLocale(this)
+//        val language =  getSharedPreferences("user", 0).getString("languageId", "")
+//        if (language == "" ||language == "0" || language == "1") window.decorView.layoutDirection= View.LAYOUT_DIRECTION_LTR
+//        else window.decorView.layoutDirection= View.LAYOUT_DIRECTION_RTL
+//    }
 }

5fde97bb7b357156ca5967715a3d5f7e4686d638
zeyad_sharo <zeyadsharo85@gmail.com>
2024-09-10 14:27:27 +0300

    chore: Update Huawei Map integration and dependencies

--- app/src/main/java/com/application/adverial/MainActivity.kt
+++ app/src/main/java/com/application/adverial/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       MapsInitializer.setApiKey("DQEDACRuLE5ygNAVgf/C/jiDIULciSgrEQuOKKATQxwiZFYsqUFTLr4CJke4SudwvutlZqfvK5OWVYZ6B16ZeM/hojk/RC6ScXsgaw==");
+       MapsInitializer.initialize(this)
         setContentView(R.layout.activity_main)
         val inputMethodManager: InputMethodManager =
             getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
